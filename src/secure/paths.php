e3c51a8de08bf2c0a69571eae0d8bae4d0cc8703
agoldstein03 <38170312+agoldstein03@users.noreply.github.com>
2021-01-21 03:13:20 -0500

    Create paths.php

---
+++ src/secure/paths.php
@@ -0,0 +1,22 @@
+<?php
+const PATHS = [
+    "en" => [
+        "/" => "index_en.html",
+        "/about-us" => "about-us_en.html",
+        "/academics" => "academics_en.html",
+        "/calendar" => "calendar_en.html",
+        "/registration" => "registration_en.html",
+        "/charitable-giving" => "charitable-giving_en.html",
+        "/contact-us" => "contact-us_en.html"
+    ],
+    "zh" => [
+        "/" => "index_zh.html",
+        "/about-us" => "about-us_zh.html",
+        "/academics" => "academics_zh.html",
+        "/calendar" => "calzhdar_zh.html",
+        "/registration" => "registration_zh.html",
+        "/charitable-giving" => "charitable-giving_zh.html",
+        "/contact-us" => "contact-us_zh.html"
+    ]
+];
+?>
