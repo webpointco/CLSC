69635fe87246aec8d99ec7aec01f6a5d868791a9
agoldstein03 <38170312+agoldstein03@users.noreply.github.com>
2021-01-22 00:24:31 -0500

    Finalize posting

--- src/secure/post.php
+++ src/secure/post.php
@@ -1,11 +1,17 @@
 <?php
 require("paths.php");
-$file = fopen(__DIR__."/../test_".PATHS["en"]["/about-us"], "w");
-$content = file_get_contents('php://input');
-$result = fwrite($file, $content);
-if ($result) {
-    echo "Successfully wrote $result bytes!"
+$data = json_decode(file_get_contents('php://input'), true);
+if (isset($data) && array_key_exists("lang", $data) && array_key_exists("page", $data) && array_key_exists("editable", $data) && array_key_exists("content", $data)
+ && array_key_exists($data["lang"], PATHS) && array_key_exists($data["page"], PATHS[$data["lang"]])) {
+    $file = fopen(__DIR__.($data["editable"] ? "/" : "/../").PATHS[$data["lang"]][$data["page"]], "w");
+    $content = $data["content"];
+    $result = fwrite($file, $content);
+    if ($result) {
+        echo "Successfully wrote $result bytes!";
+    } else {
+        echo "Failed.";
+    }
 } else {
-    echo "Failed."
+    echo "Invalid.";
 }
 ?>
