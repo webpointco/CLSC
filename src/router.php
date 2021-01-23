41182522bf7dd85c5f64bb02d6274864535171d0
agoldstein03 <38170312+agoldstein03@users.noreply.github.com>
2021-01-23 00:17:18 -0500

    Improving router

--- src/router.php
+++ src/router.php
@@ -1,12 +1,29 @@
 <?php
-require("secure/paths.php");
+const SECURE_REGEX = "/secure$/";
 const LANG_REGEX = "/en|zh/i";
+const SECURE_PATH_REGEX = "/^\/secure/";
+
+$secure = preg_match(SECURE_REGEX, __DIR__);
+if ($secure) {
+    require("paths.php");
+} else {
+    require("secure/paths.php");
+}
 
 $acceptLanguage = $_SERVER['HTTP_ACCEPT_LANGUAGE'];
 $matches = [];
-$lang = strtolower(($acceptLanguage != null && preg_match(LANG_REGEX, $_SERVER['HTTP_ACCEPT_LANGUAGE'], $matches)) ? $matches[0] : "en");
+$lang = strtolower((isset($_GET) && array_key_exists("lang", $_GET)) ? $_GET["lang"] : 
+    (isset($_COOKIE) && array_key_exists("lang", $_COOKIE) ? $_COOKIE["lang"] :
+        ($acceptLanguage != null && preg_match(LANG_REGEX, $acceptLanguage, $matches) ? $matches[0] : 
+            "en")
+    )
+);
 
-$path = $_SERVER['REQUEST_URI'];
+$path = strtok($_SERVER['REQUEST_URI'], "?");
+if ($secure) {
+    $path = preg_replace(SECURE_PATH_REGEX, "", $path);
+}
 $localPath = PATHS[$lang][$path];
+setcookie("lang", $lang, time() + (86400 * 30));
 require($localPath);
 ?>
